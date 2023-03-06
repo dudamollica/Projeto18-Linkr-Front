d501fa5058af802164b3d5106ce25a52a1edd30e
dudamollica <dudarenno@yahoo.com.br>
2023-03-06 20:50:33 -0300

    Feat: Initial Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AuthProvider from "./AppContext/auth.js";
+import GlobalStyle from "./Constants/GlobalStyle.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <AuthProvider>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+     
+        </Routes>
+      </BrowserRouter>
+      </AuthProvider>
+    </>
   );
 }
 
